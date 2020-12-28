a6cdedb2226b41cc5df977d8ae0760ba3785dcb9
Janne Huovari <janne.huovari@ptt.fi>
2020-12-28 17:35:46 +0200

    names argument for ptt_get_statfi

--- R/ptt_statfi.R
+++ R/ptt_statfi.R
@@ -4,6 +4,7 @@
 #'
 #' @param url A pxweb object or url that can be coherced to a pxweb object
 #' @param query A json string, json file or list object that can be coherced to a pxweb_query object.
+#' @param names Whether to add columns for names. "all", "none" or vector of variable names.
 #'
 #' @import pxweb
 #' @import dplyr
@@ -20,6 +21,8 @@
 #'          "Tiedot"=c("vaesto"))
 #'
 #'   pp <- ptt_get_statfi(url, query)
+#'   pp <- ptt_get_statfi(url, query, names = "none")
+#'   pp <- ptt_get_statfi(url, query, names = c("Alue"))
 #'
 #'   url <- "http://pxnet2.stat.fi/PXWeb/api/v1/fi/Kokeelliset_tilastot/nopsu/koeti_nopsu_pxt_11mx.px"
 #'   query <-
@@ -29,27 +32,33 @@
 #'                    "kausitasoitettu_euro",
 #'                    "trendi_euro"))
 #'
-#'   pp2 <- ptt_get_statfi(url, query, names = FALSE)
+#'   pp2 <- ptt_get_statfi(url, query, names = )
 
-ptt_get_statfi <- function(url, query, names = TRUE){
+ptt_get_statfi <- function(url, query, names = "all"){
   px_data <- pxweb::pxweb_get(url = url, query = query)
 
   codes_names <- px_code_name(px_data)
+
+  if (names == "all") {
+    to_name <- names(codes_names)
+  } else if (names == "none") {
+    to_name <- NULL
+  } else {
+    to_name <- names
+  }
+
 
   px_df <- as.data.frame(px_data, column.name.type = "code",
                          variable.value.type = "code") %>%
     statfitools::clean_times2() %>%
     tidyr::pivot_longer(where(is.numeric), names_to = dplyr::last(names(codes_names)), values_to = "values") %>%
-    {if (names)
-      {
-      dplyr::mutate(., across(where(is.character),
+    dplyr::mutate(across(any_of(to_name) & where(is.character),
                            ~factor(.x,
                                    levels = names(codes_names[[cur_column()]]),
                                    labels = codes_names[[cur_column()]]),
                            .names = "{.col}_name")) %>%
-        rename_with(.cols = where(is.character), ~paste0(.x, "_code"))
-      } else {.}
-    }  %>%
+    dplyr::rename_with(.cols = any_of(to_name) & where(is.character), ~paste0(.x, "_code")) %>%
+    dplyr::mutate(across(where(is.character), ~forcats::as_factor(.x))) %>%
     statfitools::clean_names() %>%
     relocate(time) %>%
     relocate(values, .after = last_col())
