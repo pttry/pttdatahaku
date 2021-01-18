25a7f013eed9f19871a7aa62ebdf1eb62d747330
Janne Huovari <janne.huovari@ptt.fi>
2021-01-18 08:52:48 +0200

    Save to teams

--- R/ptt_save_load.R
+++ R/ptt_save_load.R
@@ -1,6 +1,11 @@
 #' Saves and loads data tables
 #'
+#' For now saves to shared teams
+#' Pellervon taloustutkimus - Datapankki/Tietokanta folder.
+#' The foldet must be synced.
+#'
 #' @param x A data to save
+#' @param x_name A name of data.
 #'
 #' @examples
 #' test_dat <- data.frame(a = c(1,2))
@@ -8,12 +13,17 @@
 #' test_dat2 <- ptt_read_data("test_dat")
 
 ptt_save_data <- function(x){
-  qs::qsave(x, file = here::here("data", paste0(deparse1(substitute(x)), ".qs")))
+  qs::qsave(x, file = file.path(db_path, paste0(deparse1(substitute(x)), ".qs")))
 }
 
 #'
 #' @describeIn ptt_save_data
 #'
 ptt_read_data <- function(x_name){
-  qs::qread(file = here::here("data", paste0(x_name, ".qs")))
+
+  # path.expand(path)
+  qs::qread(file = file.path(db_path, paste0(x_name, ".qs")))
 }
+
+
+db_path <- "~/../Pellervon Taloustutkimus PTT ry/Pellervon taloustutkimus - Datapankki/Tietokanta"
