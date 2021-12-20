e0fabf386b1edaa6514eabd44ca169e170986139
Juho Alasalmi <juho.alasalmi@ptt.fi>
2021-12-20 09:48:23 +0100

    add function to print full query as r list

--- R/pxweb_utils.R
+++ R/pxweb_utils.R
@@ -60,8 +60,6 @@ pxweb_print_code_full_query_c <- function(url, time_all = TRUE, target = "clipbo
 #'
 #' @describeIn pxweb_print_full_query
 #'
-
-
 pxweb_prepare_full_query <- function(url, time_all = TRUE) {
   meta <- pxweb::pxweb_get(url)
   codes <- sapply(meta$variables, "[[", "code")
@@ -79,4 +77,16 @@ pxweb_prepare_full_query <- function(url, time_all = TRUE) {
     full_query$query[[time_position]]$selection$values <- "*"
   }
   full_query
+}
+
+#' Return full query as a list
+#'
+#' @describeIn pxweb_print_full_query
+#'
+pxweb_full_query_as_list <- function(url, time_all = TRUE) {
+
+  url <- statfi_parse_url(url)
+  full_query_r_expr <- pxweb:::pxweb_query_as_rcode(pxweb_prepare_full_query(url, time_all = time_all))
+  eval(parse(text = paste0(full_query_r_expr[-1], collapse = "\n")))
+
 }
